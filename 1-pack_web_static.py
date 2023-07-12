4cbf6c321e0127281c28beae67d2d5733cdbcb53
Fortunatus <wulnyefortunatus@gmail.com>
2023-07-12 17:58:29 +0000

    file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
-"""
-Fabric script to generate a .tgz archive from web_static folder
-"""
-from fabric.api import local
+""" Function that compress a folder """
 from datetime import datetime
-from os.path import exists
+from fabric.api import local
 
 
 def do_pack():
     """
-    Generates a .tgz archive from web_static folder
-    Returns the path of the archive if successful, None otherwise
+    must return the archive path if the archive has been correctly
+    generated. Otherwise, it should return None
     """
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive_name = "versions/web_static_{}.tgz".format(timestamp)
-
-    local("mkdir -p versions")
-    result = local("tar -cvzf {} web_static".format(archive_name))
-
-    if exists(archive_name):
-        print("web_static packed: {} -> {}Bytes".format(archive_name, result.stdout.split()[-2]))
-        return archive_name
-    else:
+    try:
+        local("mkdir -p versions")
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        rout = "versions/web_static_{}.tgz".format(date)
+        _gzip = local("tar -cvzf {} web_static".format(rout))
+        return rout
+    except Exception:
         return None
