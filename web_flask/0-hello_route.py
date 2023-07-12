ad96f25416f8927fe7086745d32b6f3d3db6e5ef
Fortunatus <wulnyefortunatus@gmail.com>
2023-07-12 17:05:18 +0000

    changes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,10 +7,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+# Define a route and its associated function
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
 
+# Run the Flask application if this file is executed directly
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
